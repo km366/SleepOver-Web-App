681947e8d89fbc8d45f14935f7b62e0c35f9eff5
kat.366 <km3723@drexel.edu>
2020-04-20 17:47:38 +0530

    package.json

--- src/app/availability/availability.component.ts
+++ src/app/availability/availability.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserDatesService } from '../UserDates.service';
-import * as firebase from 'firebase/app';
 
 
 @Component({
